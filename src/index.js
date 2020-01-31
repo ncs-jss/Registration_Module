4f9e77c2b55afd9ad33a10503c8bee4db9b15df0
kunalvishnoi <vishnoi.kunal0@gmail.com>
2020-02-01 02:21:03 +0530

    custom toast and common axios add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { withToastProvider } from "./utils/Toast";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppWithToastProvider = withToastProvider(App);
+
+ReactDOM.render(<AppWithToastProvider />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/utils/Toast/Toast.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from "react";
+
+function Toast({ children, remove, status }) {
+  const removeRef = useRef();
+  removeRef.current = remove;
+
+  useEffect(() => {
+    const duration = 5000;
+    const id = setTimeout(() => removeRef.current(), duration);
+    return () => clearTimeout(id);
+  }, []);
+
+  return (
+    <div className={`toast toast-${status}`}>
+      <div className="toast__text">{children}</div>
+      <div>
+        <button onClick={remove} className="toast__close-btn">
+          x
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Toast;
